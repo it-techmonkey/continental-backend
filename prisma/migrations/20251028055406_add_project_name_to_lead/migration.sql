7ae1c14d049b9bf4c4198db08bf5af50d3bae75a
Chinmay <chinmaybadwaik9@gmail.com>
2025-10-28 11:24:28 +0530

    feaT: migration updated

---
+++ prisma/migrations/20251028055406_add_project_name_to_lead/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "leads" ADD COLUMN     "developerName" TEXT,
+ADD COLUMN     "notes" TEXT,
+ADD COLUMN     "price" DOUBLE PRECISION,
+ADD COLUMN     "projectName" TEXT,
+ADD COLUMN     "type" TEXT;
