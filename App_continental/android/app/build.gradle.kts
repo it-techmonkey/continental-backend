ac9a41422a30b8d4e30f9c5764701270800f091d
Manas <manasgedam26@gmail.com>
2026-06-12 14:10:29 +0530

    fix: upgrade AGP 8.9.1, Gradle 8.14.2, NDK 28.2 for CI build compatibility

--- App_continental/android/app/build.gradle.kts
+++ App_continental/android/app/build.gradle.kts
@@ -11,7 +11,7 @@ import java.io.FileInputStream
 android {
     namespace = "com.continental.mobileapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
